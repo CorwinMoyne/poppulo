83fc8f254b5f83a61a1c190ce5b6a4c617537d31
Corwin Moyne <c_moyne@yahoo.com>
2022-04-28 15:17:53 +0100

    feat: add react router

--- src/index.tsx
+++ src/index.tsx
@@ -1,6 +1,7 @@
 import React from "react";
 import { createRoot } from "react-dom/client";
 import { Provider } from "react-redux";
+import { BrowserRouter as Router } from "react-router-dom";
 import { ThemeProvider } from "styled-components";
 import App from "./App";
 import { store } from "./app/store";
@@ -17,7 +18,9 @@ root.render(
       <Provider store={store}>
         <GlobalStyle />
         <ErrorBoundary>
-          <App />
+          <Router>
+            <App />
+          </Router>
         </ErrorBoundary>
       </Provider>
     </ThemeProvider>
