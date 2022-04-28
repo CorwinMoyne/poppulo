3536ddd297cac082dac301723718992ed1bdb0fc
Corwin Moyne <c_moyne@yahoo.com>
2022-04-28 14:18:02 +0100

    feat: eslint

--- src/App.tsx
+++ src/App.tsx
@@ -2,7 +2,7 @@ import * as React from "react";
 
 interface Props {}
 
-const App: React.FunctionComponent<Props> = (props) => {
+const App: React.FunctionComponent<Props> = () => {
   return <div>app</div>;
 };
 
